55aa84920190e80ac4730f800fbb7bfee8f84cf9
matheusleal55 <msilvacoutoleal@gmail.com>
2021-05-06 21:16:55 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dsvendas/controller/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsvendas/controller/SaleController.java
@@ -3,25 +3,27 @@ package com.devsuperior.dsvendas.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.dsvendas.DTO.SellerDTO;
-import com.devsuperior.dsvendas.services.SellerService;
+import com.devsuperior.dsvendas.DTO.SaleDTO;
+import com.devsuperior.dsvendas.services.SaleService;
 
 @RestController
-@RequestMapping(value = "/sellers")
+@RequestMapping(value = "/sale")
 public class SaleController {
 	
 	
 	@Autowired
-	private SellerService service;
+	private SaleService service;
 	
 	@GetMapping
-	public ResponseEntity<List<SellerDTO>> findAll() {
-		List<SellerDTO> list = service.findAll();
+	public ResponseEntity<Page<SaleDTO>> findAll(Pageable pageable) {
+		Page<SaleDTO> list = service.findAll(pageable);
 		return ResponseEntity.ok(list);
 		}
 
--- backend/src/main/java/com/devsuperior/dsvendas/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsvendas/services/SaleService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsvendas.DTO.SaleDTO;
 import com.devsuperior.dsvendas.DTO.SellerDTO;
@@ -21,6 +22,10 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	@Transactional(readOnly = true)
 	public Page<SaleDTO> findAll(Pageable pageable){
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
